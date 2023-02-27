8fb5a0bc229918ca89426163dab37055382cb594
Rick Porto <rickmarco4@hotmail.com>
2023-02-27 15:07:39 -0300

    Movimentos Possiveis da Torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -43,6 +44,15 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on the designated position.");
+		}
+		if(board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Theres is no possible moves");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece cannot be move to the target position");
 		}
 	}
 	
