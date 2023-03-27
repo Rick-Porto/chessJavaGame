4d9f1016343a738e5ceafed7e329517ae1e7b9c1
Rick Porto <rickmarco4@hotmail.com>
2023-03-27 17:47:02 -0300

    Cavalo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
@@ -197,9 +198,11 @@ public class ChessMatch {
 	
 	private void initialSetup() {	 
 	        placeNewPiece('A', 1, new Rook(board, Color.WHITE));
+	        placeNewPiece('B', 1, new Knight(board, Color.WHITE));
 	        placeNewPiece('C', 1, new Bishop(board, Color.WHITE));
 	        placeNewPiece('E', 1, new King(board, Color.WHITE));
 	        placeNewPiece('F', 1, new Bishop(board, Color.WHITE));
+	        placeNewPiece('G', 1, new Knight(board, Color.WHITE));
 	        placeNewPiece('H', 1, new Rook(board, Color.WHITE));
 	        placeNewPiece('A', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('B', 2, new Pawn(board, Color.WHITE));
@@ -211,9 +214,11 @@ public class ChessMatch {
 	        placeNewPiece('H', 2, new Pawn(board, Color.WHITE));
 
 	        placeNewPiece('A', 8, new Rook(board, Color.BLACK));
+	        placeNewPiece('B', 8, new Knight(board, Color.BLACK));
 	        placeNewPiece('C', 8, new Bishop(board, Color.BLACK));
 	        placeNewPiece('E', 8, new King(board, Color.BLACK));
 	        placeNewPiece('F', 8, new Bishop(board, Color.BLACK));
+	        placeNewPiece('G', 8, new Knight(board, Color.BLACK));
 	        placeNewPiece('H', 8, new Rook(board, Color.BLACK));
 	        placeNewPiece('A', 7, new Pawn(board, Color.BLACK));
 	        placeNewPiece('B', 7, new Pawn(board, Color.BLACK));
